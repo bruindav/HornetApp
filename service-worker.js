f00eab81047edc97b40a870e540a68882dbd971b
bruindav <davedebruin123@gmail.com>
2026-02-23 11:39:54 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
-const CACHE = 'hornet-mapper-v5-609d';
-const ASSETS = ['./','./index.html?v=609d','./app.css?v=609d','./app.js?v=609d','./manifest.webmanifest?v=609d'];
+const CACHE = 'hornet-mapper-v6-609e';
+const ASSETS = ['./','./index.html?v=609e','./app.css?v=609e','./app.js?v=609e','./manifest.webmanifest?v=609e'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); });
 self.addEventListener('fetch', e => {
