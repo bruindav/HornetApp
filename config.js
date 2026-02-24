9df469c47d2e233a3f6da92341c3a87d640ecee0
bruindav <davedebruin123@gmail.com>
2026-02-24 20:04:28 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-export const firebaseConfig = { apiKey:'<VUL-IN>', authDomain:'<VUL-IN>', projectId:'<VUL-IN>', storageBucket:'<VUL-IN>', messagingSenderId:'<VUL-IN>', appId:'<VUL-IN>' };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgRWejn5vrpFB9znGxte7a_sFWRp-xeYk",
+  authDomain: "hornet-mapper.firebaseapp.com",
+  projectId: "hornet-mapper",
+  storageBucket: "hornet-mapper.firebasestorage.app",
+  messagingSenderId: "533861029631",
+  appId: "1:533861029631:web:de718960a4a55ea8265eca"
+};
