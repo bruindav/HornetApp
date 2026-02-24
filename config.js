a769d8e442c4f09dc51570cb891c9bd2c558e45c
bruindav <davedebruin123@gmail.com>
2026-02-24 20:23:03 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCgRWejn5vrpFB9znGxte7a_sFWRp-xeYk",
   authDomain: "hornet-mapper.firebaseapp.com",
   projectId: "hornet-mapper",
-  storageBucket: "hornet-mapper.firebasestorage.app",
+  storageBucket: "hornet-mapper.appspot.com",   // <— aangepast
   messagingSenderId: "533861029631",
   appId: "1:533861029631:web:de718960a4a55ea8265eca"
 };
