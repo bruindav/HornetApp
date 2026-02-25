524ff368b323642a8bb77247b16211e2f5001394
bruindav <davedebruin123@gmail.com>
2026-02-25 17:34:02 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { firebaseConfig } from './config.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js';
-import { getFirestore, enableIndexedDbPersistence } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js';
+import { getFirestore, if(!window.__hm_persistence_set){window.__hm_persistence_set=true; enableMultiTabIndexedDbPersistence } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js';
 export const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
-enableIndexedDbPersistence(db).catch(()=>{});
+if(!window.__hm_persistence_set){window.__hm_persistence_set=true; enableMultiTabIndexedDbPersistence(db).catch(()=>{});
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-const CACHE='fix9-610r21f9';self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
+const CACHE = 'hornet-610r21f11';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    './', './index.html?v=610r21f11', './app.css?v=610r21f11', './app.js?v=610r21f11', './firebase.js?v=610r21f11', './config.js?v=610r21f11', './manifest.webmanifest?v=610r21f11', './favicon.ico'
+  ])));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
