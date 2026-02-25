ccbfb0344179791dec393cfde0543dc505348a2d
bruindav <davedebruin123@gmail.com>
2026-02-25 18:32:39 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,14 @@
-const CACHE='hornet-610r21f12';
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=610r21f12','./app.js?v=610r21f12','./app.css?v=610r21f12','./firebase.js?v=610r21f12','./config.js?v=610r21f12','./manifest.webmanifest?v=610r21f12','./favicon.ico']))) });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+const CACHE='hornet-610r21f12b';
+const ASSETS=[
+'./',
+'./index.html?v=610r21f12b',
+'./main.js?v=610r21f12b',
+'./app.css?v=610r21f12b',
+'./firebase.js?v=610r21f12b',
+'./config.js?v=610r21f12b',
+'./manifest.webmanifest?v=610r21f12b',
+'./favicon.ico'
+];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
