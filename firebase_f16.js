00fc49faf83949670958bc47110ce635b43f9d89
bruindav <davedebruin123@gmail.com>
2026-02-26 11:02:58 +0100

    Delete firebase_f16.js

--- firebase_f16.js
+++
@@ -1,7 +0,0 @@
-
-import { firebase_f16.js';
-import { initializeApp } from 'https://www.gstatic.com/firebase_f16.js/10.11.0/firebase_f16.js';
-import { getFirestore, enableMultiTabIndexedDbPersistence } from 'https://www.gstatic.com/firebase_f16.js/10.11.0/firebase_f16.js';
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-if (!window.__hm_persistence_set) { window.__hm_persistence_set = true; enableMultiTabIndexedDbPersistence(db).catch(()=>{}); }
