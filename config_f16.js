0de3b5678cae8ae5a3e8d00e4a22e1f022baf084
bruindav <davedebruin123@gmail.com>
2026-02-26 11:03:53 +0100

    Delete config_f16.js

--- config_f16.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
- apiKey: "AIzaSyCgRWejn5vrpFB9znGxte7a_sFWRp-xeYk",
- authDomain: "hornet-mapper.firebaseapp.com",
- projectId: "hornet-mapper",
- storageBucket: "hornet-mapper.appspot.com", // <— aangepast
- messagingSenderId: "533861029631",
- appId: "1:533861029631:web:de718960a4a55ea8265eca"
-};
